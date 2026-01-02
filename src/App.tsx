0c2b336e8ec82cf811ea07708778853c96fe80c1
Tatsuya <ntatsuya19971104@yahoo.co.jp>
2026-01-02 17:09:14 +0900

    デフォルトルートを検索ページにリダイレクト

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { SearchPage } from "./pages/search-page";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to={"/search"} replace />} />
         <Route path="/search" element={<SearchPage />} />
       </Routes>
     </BrowserRouter>
